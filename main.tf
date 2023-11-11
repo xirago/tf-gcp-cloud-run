0ee9b99d29949d45ec7f78f67721f3cf70a78283
xirago <xirago@gmail.com>
2023-11-11 09:39:26 +0000

    feat: Update Cloud Run service variables

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "google_cloud_run_v2_service" "this" {
-  name     = "vm-demo"
+  name     = "${var.name_prefix}-vm-demo"
   location = var.region
 
+  # TODO: Remove unfiltered PUBLIC access
   ingress = "INGRESS_TRAFFIC_ALL"
 
   template {
     containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      image = var.run_container_uri
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -9,6 +9,12 @@ variable "region" {
 }
 variable "name_prefix" {
   type        = string
-  default     = "vm-task"
+  default     = "cloud-run-public"
   description = "A task designation to be used a prefix in resource naming"
+}
+
+variable "run_container_url" {
+  type        = string
+  default     = "us-docker.pkg.dev/cloudrun/container/hello"
+  description = "URL of the Container image in Google Container Registry or Google Artifact Registry"
 }
