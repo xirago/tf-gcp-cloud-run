eab1d11a8ba04eca38c80b39686fdd14c423fb94
xirago <xirago@gmail.com>
2023-11-12 11:29:10 +0000

    test: Add GLB resources for end-to-end test
    Adds:
      - HTTP target proxy (Not HTTPS)
      - Default URL Map
      - HTTP forwarding rule (Not HTTPS)
    
    NOTE: This deployment **will not** directly expose the cloud run service yet.
    No specific IAM policy grants `invoker` yet.

--- load-balancer.tf
+++ load-balancer.tf
@@ -12,3 +12,21 @@ resource "google_compute_backend_service" "cloud_run" {
   }
 }
 
+resource "google_compute_target_http_proxy" "this" {
+  name    = "${var.name_prefix}-cloud-run"
+  url_map = google_compute_url_map.default.id
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "${var.name_prefix}-default-map"
+  default_service = google_compute_backend_service.cloud_run.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "http" {
+  name                  = "proxy-cloud-run"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.this.id
+  ip_address            = google_compute_global_address.this.id
+}
