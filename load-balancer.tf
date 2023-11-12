25c4d89c289ba06ca920e0d800cf7fbf68207e70
xirago <xirago@gmail.com>
2023-11-12 09:59:30 +0000

    feat: Add global LB backend serice

--- load-balancer.tf
+++ load-balancer.tf
@@ -3,3 +3,12 @@ resource "google_compute_global_address" "this" {
   ip_version = "IPV4"
 }
 
+resource "google_compute_backend_service" "cloud_run" {
+  name                  = "${var.name_prefix}-cloud_run"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+
+  backend {
+    group = google_compute_region_network_endpoint_group.cloud_run.id
+  }
+}
+
